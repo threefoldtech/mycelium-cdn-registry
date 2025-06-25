057620b159b602c0fd223eeb24b22086f6be15b1
Lee Smet <lee.smet@hotmail.com>
2025-06-25 20:25:33 +0200

    Add file upload encryption layout fixes
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- crates/mycdnctl/src/main.rs
+++ crates/mycdnctl/src/main.rs
@@ -1,9 +1,15 @@
 use std::{
-    fs::{File, FileType, OpenOptions}, io::Read, path::{Path, PathBuf}
+    fs::{File, FileType, OpenOptions},
+    io::Read,
+    path::{Path, PathBuf},
 };
 
-use aes_gcm::{aead::Aead, AeadInPlace, KeyInit};
+use aes_gcm::{AeadInPlace, KeyInit, aead::Aead};
 use clap::{Parser, Subcommand};
+use rand::random;
+
+// The maximum unencrypted size of one chunk of content
+const MAX_CHUNK_SIZE: u64 = 5 << 20; // 5 MiB
 
 #[derive(Parser)]
 #[command(version, about)]
@@ -28,6 +34,10 @@ enum Command {
         /// Custom name of the object. If this is not set, the leaf of the path value is used.
         #[arg(short, long)]
         name: Option<String>,
+        /// The size of chunks to generate when a file is uploaded which is larger than this
+        /// object.
+        #[arg(short, long, default_value_t = MAX_CHUNK_SIZE, value_parser = clap::value_parser!(u64).range(1<<20..5<<20))]
+        chunk_size: u64,
     },
 }
 
@@ -35,7 +45,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     match args.command {
-        Command::Upload { object, mime, name } => {
+        Command::Upload {
+            object,
+            mime,
+            name,
+            chunk_size,
+        } => {
             if !object.exists() {
                 eprintln!("{} does not exist", object.display());
                 std::process::exit(1);
@@ -48,10 +63,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
 
             let meta = if ft.is_file() {
-                upload_file(&object)?
+                upload_file(&object, chunk_size)?
             } else {
                 upload_dir(&object)?
-            }
+            };
 
             todo!();
         }
@@ -59,7 +74,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 /// Encrypt, chunk, and upload the chunks
-fn upload_file(file: &Path) -> Result<(), Box<dyn std::error::Error>> {
+fn upload_file(file: &Path, chunk_size: u64) -> Result<(), Box<dyn std::error::Error>> {
     let mut file = File::open(file)?;
     let mut content = vec![];
     file.read_to_end(&mut content)?;
@@ -67,13 +82,28 @@ fn upload_file(file: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let orig_hash = blake3::hash(&content);
 
     // TODO: Compress?
-    
-    let encryptor = aes_gcm::Aes256Gcm::new((&orig_hash.as_bytes()[..]).into());
-    // TODO: generate random nonce
-    let nonce = [0,1,2,3,4,5,6,7,8,9,10,11];
-    let encrypted_content = encryptor.encrypt(&nonce.into(), content.as_slice())?;
 
-    // TODO: Chunk 
+    // Chunk content to size. This must be done now since decryption can only happen at the
+    // beginning, so decrypting a chunk created after encryption would require the whole encrypted
+    // object regardless.
+    let mut chunks = Vec::with_capacity(content.len().div_ceil(chunk_size as usize));
+    for i in 0..chunks.capacity() {
+        chunks.push(&content[i * chunk_size as usize..(i + 1) * chunk_size as usize]);
+    }
+
+    for chunk in chunks {
+        let encryptor = aes_gcm::Aes256Gcm::new((&orig_hash.as_bytes()[..]).into());
+        let nonce: [u8; 12] = random();
+        let ciphertext = encryptor
+            .encrypt(&nonce.into(), chunk)
+            .map_err(|_| "Encryption failed")?;
+
+        // Now we can do encoding of the chunk into smaller chunks
+        //TODO:
+    }
+
+    // TODO: upload encryped chunks
+
     todo!();
 }
 
