a94275c6d2ab154438ed093ad9b472b6a442ebf0
Lee Smet <lee.smet@hotmail.com>
2025-06-29 23:18:16 +0200

    Add binary encoding and decoding of metadata
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- crates/cdn-meta/src/lib.rs
+++ crates/cdn-meta/src/lib.rs
@@ -8,7 +8,7 @@ pub type Hash = [u8; 32];
 pub type FileMetaHash = Hash;
 
 /// A blob of metadata, bincode encoded.
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, bincode::Encode, bincode::Decode)]
 pub enum Metadata {
     /// The metadata represents a [`File`].
     File(File),
@@ -16,8 +16,24 @@ pub enum Metadata {
     Directory(Directory),
 }
 
+impl Metadata {
+    pub fn to_binary(&self) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        let config = bincode::config::standard()
+            .with_big_endian()
+            .with_fixed_int_encoding();
+        Ok(bincode::encode_to_vec(self, config)?)
+    }
+
+    pub fn from_binary(input: &[u8]) -> Result<(Self, usize), Box<dyn std::error::Error>> {
+        let config = bincode::config::standard()
+            .with_big_endian()
+            .with_fixed_int_encoding();
+        Ok(bincode::decode_from_slice(input, config)?)
+    }
+}
+
 /// Metadata about a single file.
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, bincode::Encode, bincode::Decode)]
 pub struct File {
     /// The hash of the unencrypted file content. This is also used as encryption key.
     pub content_hash: Hash,
@@ -30,7 +46,7 @@ pub struct File {
 }
 
 /// Metadata about a single directory.
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, bincode::Encode, bincode::Decode)]
 pub struct Directory {
     /// A list of file hashes. This also includes an optional hash in case the file metadata is
     /// encrypted. In this case, the first hash is the hash of the encrypted content (it's key in
@@ -42,7 +58,7 @@ pub struct Directory {
 }
 
 /// Info about distribution of a single block.
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, bincode::Encode, bincode::Decode)]
 pub struct Block {
     pub shards: Vec<Location>,
     /// Offset in bytes this block is placed in the file.
@@ -56,7 +72,7 @@ pub struct Block {
 }
 
 /// Location information for shards in 0-DB
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, bincode::Encode, bincode::Decode)]
 pub struct Location {
     /// 0-DB host IP address and port.
     pub host: SocketAddr,
